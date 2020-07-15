68f16e973ba7d5d49b0b3d2e7ed4fdd04c141e49
nealchristopherb <christopherbneal@gmail.com>
2020-07-14 20:11:47 -0400

    revised upload

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Attempted  completion of Bootstrap/jQuery project.  Was able to get links all links to perform except reset of colors.  Attempted to change values in css, attempted to reload prior instance of ".box1", etc.  
+Attempted  completion of Bootstrap/jQuery project.  Was able to get links all links to perform.  
+
+Fundamental challenge was "resetColors" button.
 
 Used toggleClasses() for the first set of commands and hide() for the second set.  Show all was done with show().
--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-$(".changeBox1").click(function(){
-   
+$(".changeBox1").click(function(){   
     $(".box1").toggleClass("box1Change");
+    console.log("script in root")
 })
+
 $(".changeBox2").click(function(){
     $(".box2").toggleClass("box2Change");
 })
+
 $(".changeBox3").click(function(){
     $(".box3").toggleClass("box3Change");
 })
+
 $(".changeAll").click(function(){
-    $(".box1").css("background-color", "green");
-    $(".box2").css("background-color", "yellow");
-    $(".box3").css("background-color", "black");
+$(".box1").toggleClass("box1Change");
+$(".box2").toggleClass("box2Change");
+$(".box3").toggleClass("box3Change");
 })
 
 $(".hideBox1").click(function(){
     $(".box1").toggle();
 })
-$(".hideBox2").click(function(){
 
+$(".hideBox2").click(function(){
     $(".box2").toggle();
 })
+
 $(".hideBox3").click(function(){
     $(".box3").toggle();
 })
+
 $(".hideAll").click(function(){  
-    $(".box1, .box2, .box3").hide();})
+    $(".box1, .box2, .box3").hide();
+})
+
 $(".showAll").click(function(){  
-    $(".box1, .box2, .box3").show();})   
+    $(".box1, .box2, .box3").show();
+})   
 
 $(".resetColors").click(function(){
- console.log("click is working");})
- 
+ console.log("click is working properly");
+ $(".box1").removeClass("box1Change");
+ $(".box2").removeClass("box2Change");
+ $(".box3").removeClass("box3Change");
+})
